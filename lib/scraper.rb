59be51972ab1f1ddb08a93a8292d831c9d52d3eb
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-23 08:55:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,3 +25,6 @@ courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 courses.each do | course |
   puts course.text.strip
 end
+
+
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
